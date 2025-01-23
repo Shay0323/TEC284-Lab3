cc391f9961a139fef05b4761cdff0817b00b8b33
shay gladden <saglad1@ilstu.edu>
2025-01-23 16:16:57 -0600

    Create LightLevel.ino

---
+++ LightLevel/LightLevel.ino
@@ -0,0 +1,25 @@
+void setup() {
+  pinMode(A6, INPUT);
+  int lightlevel = analogRead(A6);
+  Serial.begin(9600);
+  Serial.println(lightlevel);
+  if (lightlevel < 100) {
+    Serial.println("Its really dark!");
+  }
+  else if (lightlevel < 200) {
+    Serial.println("Its dim in here");
+  }
+  else if (lightlevel < 700) {
+    Serial.println("Its a little bright");
+  }
+  else if (lightlevel < 1024) {
+    Serial.println("Its really bright!");
+  }
+
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
